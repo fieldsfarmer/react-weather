e7827edbff6da69d51cd54967a5a8f3fc13f18fb
fieldsfarmer <fieldsfarmer@gmail.com>
2016-11-23 09:24:41 -0500

    grid system and example component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var Examples = ()=>(<h3>Examples component!</h3>);
+var Examples = ()=>(
+  <div>
+    <h1 className="text-center">Examples</h1>
+    <p>Here are a few locations to try out:</p>
+    <ul>
+      <li>
+        <Link to='/?location=Pittsburgh'>Pittsburgh, PA</Link>
+      </li>
+      <li>
+        <Link to='/?location=Beijing'>Beijing, China</Link>
+      </li>
+      <li>
+        <Link to='/?location=Boston'>Boston, MA</Link>
+      </li>
+    </ul>
+  </div>
+);
 
 module.exports = Examples;
